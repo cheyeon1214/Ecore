d53564fad097a371133e3f7f1af34f84496c5774
kyw <kim020702@gmail.com>
2024-08-08 01:47:05 +0900

    회원가입/로그인폼

--- lib/signInUpPage/sign_up_form.dart
+++ lib/signInUpPage/sign_up_form.dart
@@ -1,25 +1,20 @@
 import 'package:ecore/signInUpPage/sign_in_form.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import '../cosntants/common_size.dart';
 import '../models/firebase_auth_state.dart';
 
 class SignUpForm extends StatefulWidget {
-  const SignUpForm({super.key});
-
   @override
-  State<SignUpForm> createState() => _SignUpFormState();
+  _SignUpFormState createState() => _SignUpFormState();
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _pwController = TextEditingController();
-  TextEditingController _cpwController = TextEditingController();
-
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  final TextEditingController _cpwController = TextEditingController();
 
   @override
   void dispose() {
@@ -34,99 +29,106 @@ class _SignUpFormState extends State<SignUpForm> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: Padding(
-        padding: const EdgeInsets.all(14),
+        padding: const EdgeInsets.all(common_gap),
         child: Form(
           key: _formKey,
           child: ListView(
             children: <Widget>[
-              SizedBox(
-                height: 16,
-              ),
+              SizedBox(height: 16),
               Image.asset('assets/images/ecore_logo.png'),
               TextFormField(
                 controller: _emailController,
                 cursorColor: Colors.black54,
-                decoration: textInputDecor('Email'),
+                decoration: textInputDecor('이메일'),
                 validator: (text) {
                   if (text != null && text.isNotEmpty && text.contains("@")) {
                     return null;
-                  }
-                  else {
-                    return '정확한 이메일 주소를 입력해주세요.';
+                  } else {
+                    return '정확한 이메일 주소를 입력해 주세요.';
                   }
                 },
               ),
-              SizedBox(
-                height: 10,
-              ),
+              SizedBox(height: common_xs_gap),
               TextFormField(
                 controller: _pwController,
                 cursorColor: Colors.black54,
                 obscureText: true,
-                decoration: textInputDecor('Password'),
+                decoration: textInputDecor('비밀번호'),
                 validator: (text) {
-                  if (text != null && text.isNotEmpty && text.length > 5) {
+                  if (text != null && text.isNotEmpty && text.length > 2) {
                     return null;
-                  }
-                  else {
-                    return '비밀번호를 확인해주세요.';
+                  } else {
+                    return '비밀번호를 입력해 주세요.';
                   }
                 },
               ),
-              SizedBox(
-                height: 10,
-              ),
+              SizedBox(height: common_xs_gap),
               TextFormField(
                 controller: _cpwController,
                 cursorColor: Colors.black54,
                 obscureText: true,
-                decoration: textInputDecor('Confirm Password'),
+                decoration: textInputDecor('비밀번호 확인'),
                 validator: (text) {
-                  if (text != null && text.isNotEmpty &&
-                      _pwController.text == text) {
+                  if (text != null && text.isNotEmpty && _pwController.text == text) {
                     return null;
-                  }
-                  else {
-                    return '입력한 값이 비밀번호와 일치하지 않네요 잘 입력해주세요';
+                  } else {
+                    return '입력한 비밀번호와 일치하지 않습니다.';
                   }
                 },
               ),
-              SizedBox(height: 12,
-              ),
+              SizedBox(height: common_s_gap),
               _submitButton(context),
-              SizedBox(height: 12,
-              ),
-              // OrDivider(),
+              SizedBox(height: common_s_gap),
 
             ],
           ),
-
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('이미 계정이 있으신가요? '),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignInForm()));
+              },
+              child: Text(
+                '로그인하기',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+
   }
 
   TextButton _submitButton(BuildContext context) {
     return TextButton(
       style: TextButton.styleFrom(
-          backgroundColor: Colors.blue,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(6))
+        backgroundColor: Color.fromRGBO(0, 0, 128, 1.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6),
+        ),
       ),
       onPressed: () {
         if (_formKey.currentState?.validate() ?? false) {
           print('Validation success!!');
           Provider.of<FirebaseAuthState>(context, listen: false).registerUser(
               context,
-              email: _emailController.text, password: _pwController.text);
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignInForm()));
+              email: _emailController.text,
+              password: _pwController.text);
         }
       },
       child: Text(
-        'Join',
-        style: TextStyle(
-            color: Colors.white
-        ),
+        '회원가입',
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
@@ -151,7 +153,7 @@ OutlineInputBorder errorInputBorder() {
       borderSide: BorderSide(
         color: Colors.redAccent,
       ),
-      borderRadius: BorderRadius.circular(12));
+      borderRadius: BorderRadius.circular(common_s_gap));
 }
 
 OutlineInputBorder activeInputBorder() {
@@ -159,7 +161,7 @@ OutlineInputBorder activeInputBorder() {
     borderSide: BorderSide(
       color: Colors.grey[300]!,
     ),
-    borderRadius: BorderRadius.circular(12),
+    borderRadius: BorderRadius.circular(common_s_gap),
   );
 }
 
@@ -180,3 +182,5 @@ const MaterialColor white = MaterialColor(
     900: Color(0x9FFFFFFF),
   },
 );
+
+
