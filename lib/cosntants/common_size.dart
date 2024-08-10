4b6c5f8265516b6cb9b1068cb0d750bed0567c5c
Jihyeoniii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-08-10 16:06:20 +0900

    Revert "회원가입/로그인폼"

--- lib/cosntants/common_size.dart
+++
@@ -1,9 +0,0 @@
-const double common_L_gap = 16.0;
-const double common_gap = 14.0;
-const double common_s_gap = 12.0;
-const double common_xs_gap = 10.0;
-const double common_xxs_gap = 8.0;
-
-
-const double avatar_size = 30.0;
-
