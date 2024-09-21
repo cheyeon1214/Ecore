e7991c4fc1c355efca7e24c2e99cb80da86c864b
kyw <kim020702@gmail.com>
2024-09-21 18:20:50 +0900

    리뷰->상품상세 연결

--- lib/my_market/my_market_reviewpage.dart
+++ lib/my_market/my_market_reviewpage.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart'; // 날짜 형식을 위해 추가
+import '../home_page/feed_detail.dart'; // FeedDetail 페이지 임포트
+import '../models/firestore/sell_post_model.dart'; // SellPostModel 임포트
 
 class MyMarketReviewPage extends StatelessWidget {
   final String marketId; // 마켓 ID를 받음
 
   const MyMarketReviewPage({Key? key, required this.marketId}) : super(key: key);
+
+  // 특정 sellPostId를 통해 Firestore에서 해당 상품 데이터를 가져오는 함수
+  Future<SellPostModel?> getSellPostByTitle(String marketId, String itemTitle) async {
+    try {
+      var querySnapshot = await FirebaseFirestore.instance
+          .collection('SellPosts')
+          .where('marketId', isEqualTo: marketId)
+          .where('title', isEqualTo: itemTitle)
+          .limit(1) // 해당 타이틀과 매칭되는 하나의 상품만 가져오기
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return SellPostModel.fromSnapshot(querySnapshot.docs.first); // 첫번째 문서 가져오기
+      }
+    } catch (e) {
+      print('Error fetching SellPost: $e');
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -101,70 +122,92 @@ class MyMarketReviewPage extends StatelessWidget {
                   itemCount: reviews.length,
                   itemBuilder: (context, index) {
                     var review = reviews[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0), // 전체 간격 조정
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // 별과 평점 부분
-                          Row(
-                            children: [
-                              Icon(Icons.star, color: Colors.amber, size: 20),
-                              SizedBox(width: 4),
-                              Text(
-                                review['rating'].toString(),
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16), // 평점 굵게
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 4),
-                          // 리뷰 내용 부분
-                          Text(
-                            review['review'],
-                            style: TextStyle(fontSize: 14),
-                          ),
-                          SizedBox(height: 8),
-                          // 주문 ID와 작성일
-                          Row(
-                            children: [
-                              Text(
-                                review['orderId'], // 주문 ID 표시
-                                style: TextStyle(color: Colors.grey, fontSize: 12),
-                              ),
-                              SizedBox(width: 16),
-                              Text(
-                                DateFormat('yyyy-MM-dd').format(
-                                  (review['timestamp'] as Timestamp).toDate(),
-                                ),
-                                style: TextStyle(color: Colors.grey, fontSize: 12),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          // 거래 상품 제목과 > 버튼
-                          Container(
-                            height: 40, // 원하는 세로 길이 설정
-                            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200], // 회색 배경
-                              borderRadius: BorderRadius.circular(8),
+
+                    return GestureDetector(
+                      onTap: () async {
+                        // Firestore에서 SellPost를 검색
+                        SellPostModel? sellPost = await getSellPostByTitle(marketId, review['itemTitle']);
+
+                        // 상품이 존재하면 FeedDetail로 이동
+                        if (sellPost != null) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => FeedDetail(sellPost: sellPost),
                             ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center, // 세로축 중앙 정렬
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween, // 좌우 정렬
+                          );
+                        } else {
+                          // 오류 처리: 해당 상품을 찾지 못한 경우
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('해당 상품을 찾을 수 없습니다.')),
+                          );
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // 별과 평점 부분
+                            Row(
                               children: [
-                                Expanded(
-                                  child: Text(
-                                    '거래상품  ${review['itemTitle']}', // 거래 상품 제목
-                                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                                  ),
+                                Icon(Icons.star, color: Colors.amber, size: 20),
+                                SizedBox(width: 4),
+                                Text(
+                                  review['rating'].toString(),
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16), // 평점 굵게
                                 ),
-                                Icon(Icons.arrow_forward_ios, size: 16), // > 버튼 크기 조정
                               ],
                             ),
-                          ),
-                          SizedBox(height: 8), // 다음 리뷰와의 간격을 유지하기 위한 여백
-                        ],
+                            SizedBox(height: 4),
+                            // 리뷰 내용 부분
+                            Text(
+                              review['review'],
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            SizedBox(height: 8),
+                            // 주문 ID와 작성일
+                            Row(
+                              children: [
+                                Text(
+                                  review['orderId'], // 주문 ID 표시
+                                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                                ),
+                                SizedBox(width: 16),
+                                Text(
+                                  DateFormat('yyyy-MM-dd').format(
+                                    (review['timestamp'] as Timestamp).toDate(),
+                                  ),
+                                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 8),
+                            // 거래 상품 제목과 > 버튼
+                            Container(
+                              height: 40, // 원하는 세로 길이 설정
+                              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200], // 회색 배경
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center, // 세로축 중앙 정렬
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween, // 좌우 정렬
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      '거래상품  ${review['itemTitle']}', // 거래 상품 제목
+                                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Icon(Icons.arrow_forward_ios, size: 16), // > 버튼 크기 조정
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 8), // 다음 리뷰와의 간격을 유지하기 위한 여백
+                          ],
+                        ),
                       ),
                     );
                   },
@@ -177,5 +220,3 @@ class MyMarketReviewPage extends StatelessWidget {
     );
   }
 }
-
-
